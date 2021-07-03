67a073197b49af05f2fff99f8c2adb874b7322aa
golu9571 <76936595+golu9571@users.noreply.github.com>
2021-07-03 09:55:02 +0530

    Update sudo.py

--- tg_bot/modules/sudo.py
+++ tg_bot/modules/sudo.py
@@ -6,7 +6,7 @@ from telegram import Update, Bot
 from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 
-from tg_bot import dispatcher, SUDO_USERS, OWNER_USERNAME, OWNER_ID
+from tg_bot import dispatcher, LODU_USERS, OWNER_USERNAME, OWNER_ID
 from tg_bot.modules.helper_funcs.extraction import extract_user
 from tg_bot.modules.helper_funcs.chat_status import bot_admin
 
@@ -23,18 +23,18 @@ def lodupromote(bot: Bot, update: Update, args: List[str]):
         return ""
         
     if int(user_id) == OWNER_ID:
-        message.reply_text("The specified user is my owner! No need add him to SUDO_USERS list!")
+        message.reply_text("The specified user is my owner! No need add him to LODU_USERS list!")
         return ""
         
-    if int(user_id) in SUDO_USERS:
+    if int(user_id) in LODU_USERS:
         message.reply_text("The user is already a sudo user.")
         return ""
     
     with open("sudo_users.txt","a") as file:
         file.write(str(user_id) + "\n")
     
-    SUDO_USERS.append(user_id)
-    message.reply_text("Succefully added to SUDO user list!")
+    LODU_USERS.append(user_id)
+    message.reply_text("Succefully added to LODU user list!")
         
     return ""
 
@@ -49,10 +49,10 @@ def sudodemote(bot: Bot, update: Update, args: List[str]):
         return ""
 
     if int(user_id) == OWNER_ID:
-        message.reply_text("The specified user is my owner! I won't remove him from SUDO_USERS list!")
+        message.reply_text("The specified user is my owner! I won't remove him from LODU_USERS list!")
         return ""
     
-    if user_id not in SUDO_USERS:
+    if user_id not in LODU_USERS:
         message.reply_text("{} is not a sudo user".format(user_id))
         return ""
 
@@ -63,22 +63,22 @@ def sudodemote(bot: Bot, update: Update, args: List[str]):
             if not int(user) == user_id:
                 file.write(str(user) + "\n")
 
-    SUDO_USERS.remove(user_id)
-    message.reply_text("Succefully removed from SUDO user list!")
+    LODU_USERS.remove(user_id)
+    message.reply_text("Succefully removed from LODU user list!")
     
     return ""
 
 
 __help__ = """
 *Bot owner only:*
- - /lodupromote: promotes the user to SUDO USER
- - /sudodemote: demotes the user from SUDO USER
+ - /lodupromote: promotes the user to LODU USER
+ - /sudodemote: demotes the user from LODU USER
 """
 
-__mod_name__ = "SUDO"
+__mod_name__ = "LODU"
 
 LODUPROMOTE_HANDLER = CommandHandler("LODUPROMOTE", LODUPROMOTE, pass_args=True, filters=Filters.user(OWNER_ID))
-SUDODEMOTE_HANDLER = CommandHandler("sudodemote", sudodemote, pass_args=True, filters=Filters.user(OWNER_ID))
+LODUDEMOTE_HANDLER = CommandHandler("sudodemote", sudodemote, pass_args=True, filters=Filters.user(OWNER_ID))
 
 dispatcher.add_handler(LODUPROMOTE_HANDLER)
-dispatcher.add_handler(SUDODEMOTE_HANDLER)
+dispatcher.add_handler(LODUDEMOTE_HANDLER)
